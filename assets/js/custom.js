a118667a3656241fe91b5e8565085c8cdd840782
pasindulsan <139847616+pasindulsan@users.noreply.github.com>
2024-04-20 14:59:18 +0530

    Tourism Of Uva

---
+++ assets/js/custom.js
@@ -0,0 +1,37 @@
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+          
+       
+            // PRETTYPHOTO FUNCTION 
+
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+          
+            /*====================================
+               WRITE YOUR SCRIPTS BELOW 
+           ======================================*/
+
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+
